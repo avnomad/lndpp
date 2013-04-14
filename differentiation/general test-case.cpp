1caf0c3eee3e8f12d35006f59ce0e4d57e3d684c
Vaptistis Anogeianakis <nomad@cornercase.gr>
2013-04-14 20:36:11 +0300

    Updated my e-mail address.

--- differentiation/general test-case.cpp
+++ differentiation/general test-case.cpp
@@ -1,21 +1,19 @@
+//	Copyright (C) 2010, 2011 Vaptistis Anogeianakis <nomad@cornercase.gr>
 /*
- *  Copyright (C) 2010,2011 Anogeianakis Vaptistis
+ *	This file is part of λ&d++.
  *
+ *	λ&d++ is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
  *
- *  This file is part of λ&d++.
+ *	λ&d++ is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
  *
- *  λ&d++ is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  λ&d++ is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with λ&d++.  If not, see <http://www.gnu.org/licenses/>.
+ *	You should have received a copy of the GNU General Public License
+ *	along with λ&d++.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
